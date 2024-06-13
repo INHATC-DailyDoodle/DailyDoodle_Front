928633851a4e1443051eac262819d0e40aa89a92
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-06-13 22:11:03 +0900

    Feat : mydiary 완성

--- src/screen/MyDiary.js
+++ src/screen/MyDiary.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MyDiary = () => {
+  const [diaries, setDiaries] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user_id');
+
+    if (!user_id) {
+      console.error('User ID not found');
+      return;
+    }
+
+    fetch(`http://localhost:8000/api/diary/list/?user_id=${user_id}`)
+      .then(response => response.json())
+      .then(data => setDiaries(data))
+      .catch(error => console.error('Error fetching diaries:', error));
+  }, []);
+
   return (
     <div>
       <h2>My Diary Page</h2>
-      {/* My Diary 페이지에 해당하는 내용 */}
+      <div>
+        {diaries.length > 0 ? (
+          diaries.map(diary => (
+            <div key={diary.id}>
+              <h3>{new Date(diary.date).toLocaleDateString()}</h3>
+              <p>{diary.text}</p>
+              <p>Mood: {diary.mood}</p>
+            </div>
+          ))
+        ) : (
+          <p>No diary entries found.</p>
+        )}
+      </div>
     </div>
   );
 }
