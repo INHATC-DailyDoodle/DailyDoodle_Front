b0d83a7dfc17d0e746e6212226268bbdd2ee23ad
Koseohyeon <ksh030818@naver.com>
2024-06-13 22:38:34 +0900

    일기 리스트
    일기 리스트

--- src/screen/MyDiary.js
+++ src/screen/MyDiary.js
@@ -18,23 +18,65 @@ const MyDiary = () => {
   }, []);
 
   return (
-    <div>
-      <h2>My Diary Page</h2>
-      <div>
-        {diaries.length > 0 ? (
-          diaries.map(diary => (
-            <div key={diary.id}>
-              <h3>{new Date(diary.date).toLocaleDateString()}</h3>
-              <p>{diary.text}</p>
-              <p>Mood: {diary.mood}</p>
+    <div style={styles.container}>
+      <h2 style={styles.heading}>My Diary Page</h2>
+      {diaries.length > 0 ? (
+        diaries.map(diary => (
+          <div key={diary.id} style={styles.diaryItem}>
+            <div style={styles.diaryHeader}>
+            <h3>{new Date(diary.created_at).toLocaleDateString()}</h3>
             </div>
-          ))
-        ) : (
-          <p>No diary entries found.</p>
-        )}
-      </div>
+            <p style={styles.diaryText}>{diary.text}</p>
+            <p style={styles.diaryMood}>Mood: {diary.mood}</p>
+          </div>
+        ))
+      ) : (
+        <p style={styles.noEntries}>No diary entries found.</p>
+      )}
     </div>
   );
 }
 
 export default MyDiary;
+
+const styles = {
+  container: {
+    width: '80%',
+    margin: '20px auto',
+    padding: '20px',
+    //backgroundColor: '#f0f0f0',
+    borderRadius: '8px',
+    //boxShadow: '0 2px 4px rgba(0, 0, 0, 0.0)',
+  },
+  heading: {
+    fontSize: '2rem',
+    color: '#333',
+    marginBottom: '20px',
+  },
+  diaryItem: {
+    marginBottom: '20px',
+    padding: '20px',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)', 
+    border: '1px solid #e0e0e0',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  },
+  diaryHeader: {
+    backgroundColor: 'rgba(135, 206, 235, 0.3)', 
+    padding: '10px',
+    borderRadius: '10px',
+    
+  },
+  diaryText: {
+    color: '#666',
+    lineHeight: '1.6',
+  },
+  diaryMood: {
+    color: '#666',
+  },
+  noEntries: {
+    textAlign: 'center',
+    fontStyle: 'italic',
+    color: '#999',
+  },
+};
